bc60d1a22f9e527d744244c3b5416d302e4d63ce
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2025-07-16 08:32:02 +0900

    chore: Setup eslint and prettier (#48)
    * chore: Setup eslint and prettier
    
    * Update codes based on review comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default defineConfig([
+  {
+    extends: compat.extends('next/core-web-vitals', 'next/typescript'),
 
-export default eslintConfig;
+    plugins: {
+      prettier,
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+]);
