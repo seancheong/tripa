eb561ac7f065dd13d17b5337120711c73cf68add
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2025-08-02 19:46:52 +0900

    Update code based on PR comment

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        hostname: 'lh*.googleusercontent.com',
         port: '',
         pathname: '/**',
       },
